b68b76eead89b9a10f22fa6f1e49ab0b480619c5
Georgi Dimov <georgidimov.work@gmail.com>
2026-02-09 16:02:53 +0200

    Creating model form MiniVGG architecture

---
+++ fruit_veg_cnn/models/model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+class MiniVGG(nn.Module):
+    def __init__(self, num_classes=10):
+        super(MiniVGG, self).__init__()
+        
+    
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        
+        self.block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+    
+        self.block4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 6 * 6, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5), 
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.classifier(x)
+        return x
